d43bf3d8eebacf8e6b314ec99cdea37d59b50b72
andrei <you@example.com>
2022-11-14 17:50:07 +0200

    wip canva

--- tiroyal/client/js/home-canvas.js
+++ tiroyal/client/js/home-canvas.js
@@ -3,14 +3,23 @@ const canvas = document.getElementById("viewport");
 const context = canvas.getContext('2d');
 
 const player = new Image();
+const enemyWeak = new Image();
+const enemyPowerful = new Image();
 
-make_base();
+initializeCanvas();
 
-function make_base() {
+function initializeCanvas() {
     player.src = 'assets/game/player.png';
     player.onload = drawImage;
 }
 
 function drawImage() {
-    context.drawImage(player, 50, 50);
+    context.drawImage(player, 285, 470);
+}
+
+function onKeyPress() {
+    const canvas = document.getElementById("viewport");
+    const context = canvas.getContext('2d');
+    // check if I have a player item
+    // if yes, update player item position according to the pressed key
 }
